ab1341ce2bbe5018ffc9a2feed12da7fb5fc37b0
gpeleritti <gpeleritti@sparkdigital.com>
2018-07-18 20:27:51 -0300

    adding author by id query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,7 +7,13 @@ let books = [
     {name:'Name of the wind', genre:'Fantasy',  id:'1', description:'Name of the wind some description'},
     {name:'The Final Empire', genre:'Fantasy',  id:'2', description:'The Final Empire some description'},
     {name:'The Long Earth', genre:'Scy-Fi',  id:'3', description:'The Long Earth some description'}
-]
+];
+
+let authors = [
+    {name:'author1', id:'1'},
+    {name:'author2', id:'2'},
+    {name:'author3', id:'3'}
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -37,6 +43,14 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 //code to get data from database
                 return _.find(books, {id:args.id});
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //code to get data from database
+                return _.find(authors, {id:args.id});
             }
         }
     }
