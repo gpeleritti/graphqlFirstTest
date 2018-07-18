bad9f4dcf97ccb9252c1cf36472b8453542da368
gpeleritti <gpeleritti@sparkdigital.com>
2018-07-18 20:19:46 -0300

    Book by id request

---
+++ server/schema/schema.js
@@ -0,0 +1,47 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+let books = [
+    {name:'Name of the wind', genre:'Fantasy',  id:'1', description:'Name of the wind some description'},
+    {name:'The Final Empire', genre:'Fantasy',  id:'2', description:'The Final Empire some description'},
+    {name:'The Long Earth', genre:'Scy-Fi',  id:'3', description:'The Long Earth some description'}
+]
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString},
+        description: {type: GraphQLString}
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString}
+    })
+});
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //code to get data from database
+                return _.find(books, {id:args.id});
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
